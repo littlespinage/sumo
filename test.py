935d0a34ef47ef3779bc18b9d4cc5e5f8f1be4d5
littlespinage <littlespinag@gmail.com>
2020-06-18 15:16:12 +0200

    test.py -- python file

---
+++ test.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+
+def run():
+    sum = 0.0
+    for line in open("test.txt","r"):
+        sum = sum + float(line)
+        print(sum)
+
+if __name__ == "__main__":
+    run()
+
